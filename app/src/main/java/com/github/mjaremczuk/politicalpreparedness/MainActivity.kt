19e5e2cc37230c7078e0dc47e3c432f611f30eb6
mjaremczuk <mjaremczuk@users.noreply.github.com>
2024-01-17 20:12:26 +0100

    Release 1.4 to main #21

--- app/src/main/java/com/github/mjaremczuk/politicalpreparedness/MainActivity.kt
+++ app/src/main/java/com/github/mjaremczuk/politicalpreparedness/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.github.mjaremczuk.politicalpreparedness
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.privacy_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.privacy_policy_item -> {
+                openBrowser("https://github.com/mjaremczuk/Political-Preparedness/blob/develop/Privacy-Policy.md")
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun openBrowser(url: String) {
+        val uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        this.startActivity(intent)
     }
 }
